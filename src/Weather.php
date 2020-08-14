0d0557f0f7947239eb0bc0ce9deb14ae1787b6ec
ming <1960588251@qq.com>
2020-08-15 07:24:49 +0800

    update

--- src/Weather.php
+++ src/Weather.php
@@ -3,7 +3,6 @@
 /*
  * This file is part of the Jeffmingup/weather.
  *
- * (c) Jeffmingup <i@Jeffmingup.me>
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
